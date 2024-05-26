671bc00b06fe1729f9fc2ef32d009605444b71b8
Yasemin Toraman <yaseminntoraman@gmail.com>
2024-05-26 16:13:02 +0300

    Update Chat.js

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,68 +1,68 @@
 import { useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
-import { GiftedChat } from "react-native-gifted-chat";
-
-import {
-  onSnapshot,doc, setDoc
-} from "firebase/firestore";
-
-import { database } from "../config/firebase";
-
-import { auth } from "../config/firebase";
-
+import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
+import { onSnapshot, doc, setDoc } from "firebase/firestore";
+import { database, auth } from "../config/firebase";
 
 const Chat = () => {
+    const route = useRoute();
+    const [messages, setMessages] = useState([]);
+    const [uid, setUID] = useState("");
+    const [name, setName] = useState("");
 
-  const route = useRoute();
+    useEffect(() => {
+        const unsubscribeAuth = auth.onAuthStateChanged((user) => {
+            setUID(user?.uid);
+            setName(user?.displayName);
+        });
+        return () => unsubscribeAuth();
+    }, []);
 
-  const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        const unsubscribeSnapshot = onSnapshot(
+            doc(database, "chats", route.params.chatId),
+            (snapshot) => {
+                setMessages(snapshot.data()?.messages ?? []);
+            }
+        );
+        return () => unsubscribeSnapshot();
+    }, [route.params.chatId]);
 
-  const [uid, setUID] = useState("");
-  const [name, setName] = useState("");
+    const onSend = async (newMessages = []) => {
+        const updatedMessages = GiftedChat.append(messages, newMessages);
+        await setDoc(
+            doc(database, "chats", route.params.chatId),
+            { messages: updatedMessages },
+            { merge: true }
+        );
+    };
 
-  useEffect(() => {
-    const unsubscribeAuth = auth.onAuthStateChanged((user) => {
-      setUID(user?.uid);
-      setName(user?.displayName);
-    });
-    return () => unsubscribeAuth();
-  }, []);
+    const renderInputToolbar = (props) => {
+        return (
+            <InputToolbar
+                {...props}
+                containerStyle={{ backgroundColor: "#fdf5ed" }}
+            />
+        );
+    };
 
-  useEffect(() => {
-    const unsubscribeSnapshot = onSnapshot(
-      doc(database, "chats", route.params.chatId),
-      (snapshot) => {
-        setMessages(snapshot.data()?.messages ?? []);
-      }
+    return (
+        <View style={{ flex: 1, backgroundColor: "#fdf5ed" }}>
+            <GiftedChat
+                messages={messages.map((x) => ({
+                    ...x,
+                    createdAt: x.createdAt?.toDate(),
+                }))}
+                onSend={(messages) => onSend(messages)}
+                user={{
+                    _id: uid,
+                    name: name,
+                }}
+                renderInputToolbar={renderInputToolbar}
+            />
+        </View>
     );
-    return () => unsubscribeSnapshot();
-  }, [route.params.chatId]);
-
-  const onSend = async (newMessages = []) => {
-    const updatedMessages = GiftedChat.append(messages, newMessages);
-    await setDoc(
-      doc(database, "chats", route.params.chatId),
-      { messages: updatedMessages },
-      { merge: true }
-    );
-  };
-
-  return (
-    <View style={{ flex: 1, backgroundColor: "white" }}>
-      <GiftedChat
-        messages={messages.map((x) => ({
-          ...x,
-          createdAt: x.createdAt?.toDate(),
-        }))}
-        onSend={(messages) => onSend(messages)}
-        user={{
-          _id: uid,
-          name: name,
-        }}
-      />
-    </View>
-  );
 };
 
 export default Chat;
